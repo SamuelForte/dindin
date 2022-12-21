76d1c300f4761b59d9b57cf98da15f891e2367b7
Samuel Forte <samuelforte97@gmail.com>
2022-12-21 18:15:07 -0300

    feat: implements navbar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,38 @@
+
+import Link from "./Link";
+
+import Logo from "../../assets/images/logo.png";
+
+import "./styles.css";
+function Header(){
+    return (
+        <header>
+           
+           <div className="container">
+        
+            <div className="logo">
+
+            <img src={Logo}  alt="Logo do dindin"/>
+            <h2>dindin</h2>
+            
+            </div>
+
+            <div className="navegacao">
+
+            <nav>
+                <Link texto = "Cursos" redirect = "cursos"/>
+                <Link texto = "Blog" redirect = "blog"/>
+                <Link texto = "Contatos" redirect = "contatos"/>
+
+            </nav>
+
+            </div>
+           
+           </div>
+            
+
+            
+        </header>
+    );
+}
+export default Header;
