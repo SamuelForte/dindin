f52dd062a51c41fd0e1765c5bfa2c4b5e3b5c31e
Samuel Forte <samuelforte97@gmail.com>
2022-12-31 18:52:07 -0300

    feat: section cursos

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,8 @@
 import Navbar from "../Navbar";
 import Banner from "./Banner"
+import InvestmentImg from "../../assets/images/image3.png"
+import SavingsImg from "../../assets/images/image2.png"
+import IndependenceImg from "../../assets/images/image4.png"
 import "./styles.css";
 
 export default function Home () {
@@ -8,6 +11,34 @@ export default function Home () {
      <Navbar/>
      <Banner/>
      <section className="cursos">
+      <div className="card">
+        <img className="investimentImg" src={InvestmentImg} alt="images of Investment"/>
+        <h2 className="card__title">Investimento para iniciantes</h2>
+        <p className="card__description">Duis aute irure dolor in reprehenderit in voluptate velit cillum dolore eu fugiat nulla pariatur.</p>
+        <button className="card__button">
+          <span> começar agora</span>
+        </button>
+
+      </div>
+      <div className="card">
+        <img className="investimentImg" src={SavingsImg} alt="images of Investment"/>
+        <h2 className="card__title">Poupando e Rendendo</h2>
+        <p className="card__description">Duis aute irure dolor in reprehenderit in voluptate velit cillum dolore eu fugiat nulla pariatur.</p>
+        <button className="card__button">
+          <span> começar agora</span>
+        </button>
+
+      </div>
+
+      <div className="card">
+        <img className="investimentImg" src={IndependenceImg} alt="images of Investment"/>
+        <h2 className="card__title">Independência Financeira</h2>
+        <p className="card__description">Duis aute irure dolor in reprehenderit in voluptate velit cillum dolore eu fugiat nulla pariatur.</p>
+        <button className="card__button">
+          <span> começar agora</span>
+        </button>
+
+      </div>
 
      </section>
     </div>
